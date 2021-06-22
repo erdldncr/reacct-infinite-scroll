304b4f16dcd5b4b3340ea196d42aa3fd75616fd1
erdldncr <erdldncr@gmail.com>
2021-06-22 15:35:42 +0100

    project

---
+++ config/server.js
@@ -0,0 +1,6 @@
+module.exports={
+    APPLICATION_ID:process.env.APPLICATION_ID|| 'w0WWtmlcwkbZiTZBwib7WcHz7bf9DazEzWeCnqTFwU0',
+    SECRET:process.env.SECRET||'_zR4fMEViIxy_EI2RouD9GZfWRQnSh9i2GqVJ2BF6-I',
+    CALLBACK_URL:process.env.CALLBACK_URL || 'http://localhost:3000'
+
+}
